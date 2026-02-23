c7d8c2fd8f7a8825eb453a5a4158177307fca34a
HanifMahendra <hanifwdyt4@gmail.com>
2026-02-23 22:47:48 +0700

    Code Quality Fix

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product findById(String id);
-    public Product update(Product updatedProduct);
-    public void delete(String id);
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String id);
+    Product update(Product updatedProduct);
+    void delete(String id);
 }
